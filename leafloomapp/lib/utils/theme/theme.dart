2975653bcd408f3bbf8943b2d4c42ba0dc0e3609
M. Nur Aqil Bahri <89963229+Cill19@users.noreply.github.com>
2024-05-17 21:57:03 +0700

    theme done
    theme (done)

--- leafloomapp/lib/utils/theme/theme.dart
+++ leafloomapp/lib/utils/theme/theme.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:leafloom/utils/theme/custom_themes/text_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/appbar_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/bottom_sheet_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/checkbox_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/chip_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/elevated_button_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/outlined_button_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/text_field_theme.dart';
+import 'package:leafloom/utils/theme/custon_themes/text_theme.dart';
 
 class LAppTheme {
   LAppTheme._();
 
+  //light theme
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Schyler',
@@ -11,7 +19,15 @@ class LAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: LTextTheme.lightTheme,
+    chipTheme: LChipTheme.lightChipTheme,
+    appBarTheme: LAppBarTheme.lightAppBarTheme,
+    checkboxTheme: LCheckboxTheme.lightCheckboxTheme,
+    bottomSheetTheme: LBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: LElevatedButtonTheme.lightElevatedButtonTheme,
+    inputDecorationTheme: LTextFromFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: LOutlinedButtonTheme.lightOutlinedButtonTheme,
   );
+  //dark theme
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
     fontFamily: 'Schyler',
@@ -19,5 +35,12 @@ class LAppTheme {
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: LTextTheme.darkTheme,
+    chipTheme: LChipTheme.darkChipTheme,
+    appBarTheme: LAppBarTheme.darkAppBarTheme,
+    checkboxTheme: LCheckboxTheme.darkCheckboxTheme,
+    bottomSheetTheme: LBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: LElevatedButtonTheme.darkElevatedButtonTheme,
+    inputDecorationTheme: LTextFromFieldTheme.lightInputDecorationTheme,
+    outlinedButtonTheme: LOutlinedButtonTheme.darkOutlinedButtonTheme,
   );
 }
