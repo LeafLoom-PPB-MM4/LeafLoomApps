155b8c04b160356de60e4d5582ed09eccba3fe7b
Aisyah Hasna Aulia <asyhasnaa03@gmail.com>
2024-05-27 03:30:32 +0700

    Add icon constans

--- leafloomapp/lib/utils/constants/icons_constans.dart
+++ leafloomapp/lib/utils/constants/icons_constans.dart
@@ -1,7 +1,31 @@
 class IconsConstant {
-  static const String email = 'assets/images/email.svg';
-  static const String lock = 'assets/images/lock.svg';
-  static const String show = 'assets/images/show.svg';
-  static const String hide = 'assets/images/hide.svg';
-  static const String arrowLeft = 'assets/images/arrow-left.svg';
+  static const String email = 'assets/icons/email.svg';
+  static const String lock = 'assets/icons/lock.svg';
+  static const String show = 'assets/icons/show.svg';
+  static const String hide = 'assets/icons/hide.svg';
+  static const String arrowLeft = 'assets/icons/arrow-left.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String cart = 'assets/icons/shopping-cart.svg';
+  static const String notification = 'assets/icons/notification.svg';
+  static const String pakaian = 'assets/icons/pakaian.svg';
+  static const String tas = 'assets/icons/tas.svg';
+  static const String sepatu = 'assets/icons/sepatu.svg';
+  static const String lainnya = 'assets/icons/lainnya.svg';
+  static const String home = 'assets/icons/home.svg';
+  static const String homeFill = 'assets/icons/home-bold.svg';
+  static const String produk = 'assets/icons/produk.svg';
+  static const String produkFill = 'assets/icons/produk-bold.svg';
+  static const String profile = 'assets/icons/profil.svg';
+  static const String profileFill = 'assets/icons/profil-bold.svg';
+  static const String profileCircle = 'assets/icons/profil-circle.svg';
+  static const String arrowNext = 'assets/icons/arrow-circle-right.svg';
+  static const String arrowDown = 'assets/icons/arrow-down.svg';
+  static const String arrowTop = 'assets/icons/arrow-top.svg';
+  static const String tentangKami = 'assets/icons/tag-user.svg';
+  static const String layanan = 'assets/icons/message-question.svg';
+  static const String privacy = 'assets/icons/privacy.svg';
+  static const String logout = 'assets/icons/logout.svg';
+  static const String instagram = 'assets/icons/instagram.svg';
+  static const String facebook = 'assets/icons/facebook.svg';
+  static const String tiktok = 'assets/icons/tiktok.svg';
 }
