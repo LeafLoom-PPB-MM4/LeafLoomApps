36026e4a1d3fb6c0cc440eca0cd5713df60e9cdc
M. Nur Aqil Bahri <89963229+Cill19@users.noreply.github.com>
2024-05-29 12:21:34 +0700

    perbaikan dikit

--- leafloomapp/lib/main.dart
+++ leafloomapp/lib/main.dart
@@ -38,6 +38,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Leafloom',
       initialRoute: '/onboarding',
       getPages: [
